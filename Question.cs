680e41c48c376c8be975d67d800044387d84c0a4
AliNag0 <alinanagoeva307@gmail.com>
2025-05-28 22:50:31 +0300

    Add files via upload

---
+++ Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjects;
+
+public class Question
+{
+    public string Text { get; set; }
+    public string[] Answers { get; set; } = new string[4];
+    public int CorrectAnswer { get; set; }
+}
